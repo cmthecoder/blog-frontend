e04ba1fbd7aaf09d490759fa32e85c339cfea112
Chris Mems <christopher.mems@.com>
2023-04-29 16:23:19 -0500

    create user context

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 const Header = () => {
+  const {setUserInfo, userInfo} = useContext(UserContext)
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/profile", {
+          credentials: "include",
+        });
+        const userInfo = await response.json();
+        setUserInfo(userInfo);
+      } catch (error) {
+        console.error("Error fetching user info:", error);
+      }
+    };
+
+    fetchUserInfo();
+  }, [setUserInfo]);
+
+  const logout = () => {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    })
+    setUserInfo(null)
+  }
+
+  const username = userInfo?.username
+
   return (
     <header>
-      <Link to='/' className="logo">My Blog</Link>
+      <Link to="/" className="logo">
+        My Blog
+      </Link>
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <Link onClick={logout}>Logout</Link>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [redirect, setRedirect] = useState(false);
+  const {setUserInfo} = useContext(UserContext)
 
   const login = async (e) => {
     e.preventDefault();
@@ -17,7 +19,10 @@ const Login = () => {
     });
 
     if (response.ok) {
-      setRedirect(true);
+      response.json().then(userInfo => {
+        setUserInfo(userInfo)
+        setRedirect(true);
+      })
     } else {
       alert("Wrong Credentials");
     }
